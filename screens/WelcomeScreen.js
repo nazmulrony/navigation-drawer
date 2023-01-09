574a2cd5fcfa02a557906aba07e5b08bacf9b91c
Shahadat_Robin <robin@corexlab.dev>
2023-01-09 18:43:03 +0600

    welcome screen added

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const WelcomeScreen = () => {
     return (
-        <View>
-            <Text>Welcome</Text>
+        <View style={styles.container}>
+            <Text>
+                This is <Text style={styles.highlight}>Welcome</Text> screen
+            </Text>
         </View>
     );
 };
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    highlight: {
+        color: "red",
+    },
+});
