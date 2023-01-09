5f4beeb288cef6c8f677998291deb0fa5145a665
Shahadat_Robin <robin@corexlab.dev>
2023-01-09 19:03:44 +0600

    header style added

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 import UserScreen from "./screens/UserScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 
@@ -7,27 +8,30 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Drawer.Navigator
-                screenOptions={{
-                    headerStyle: { backgroundColor: "#038142" },
-                    headerTintColor: "white",
-                    drawerActiveBackgroundColor: "#6bffb5",
-                    drawerActiveTintColor: "#066536",
-                    drawerStyle: { backgroundColor: "#d2fee8" },
-                }}
-            >
-                <Drawer.Screen
-                    name="Welcome"
-                    component={WelcomeScreen}
-                    options={{ drawerLabel: "Welcome Screen" }}
-                />
-                <Drawer.Screen
-                    name="User"
-                    component={UserScreen}
-                    options={{ drawerLabel: "User Screen" }}
-                />
-            </Drawer.Navigator>
-        </NavigationContainer>
+        <>
+            <StatusBar style="light" />
+            <NavigationContainer>
+                <Drawer.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: "#038142" },
+                        headerTintColor: "white",
+                        drawerActiveBackgroundColor: "#6bffb5",
+                        drawerActiveTintColor: "#066536",
+                        drawerStyle: { backgroundColor: "#d2fee8" },
+                    }}
+                >
+                    <Drawer.Screen
+                        name="Welcome"
+                        component={WelcomeScreen}
+                        options={{ drawerLabel: "Welcome Screen" }}
+                    />
+                    <Drawer.Screen
+                        name="User"
+                        component={UserScreen}
+                        options={{ drawerLabel: "User Screen" }}
+                    />
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </>
     );
 }
